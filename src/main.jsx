3660807a872101c37042d7d758cab3360f2eedf6
Daniel Denkha <danialdnkha@gmail.com>
2023-11-11 22:35:24 +1100

    handling not found page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './styles/base.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/Root';
+import ErrorPage from './routes/ErrorPage';
 
 const route = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
