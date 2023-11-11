4984ff43d4327249c689c71fcb5df44eed420ef4
Daniel Denkha <danialdnkha@gmail.com>
2023-11-12 02:38:39 +1100

    styling header desktop, tablet

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,37 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import logo from '../assets/shared/logo.svg';
 function Root() {
+  const [background, setBackground] = useState('home');
+  const [dataVisible, setDataisible] = useState(true);
+  console.log(dataVisible);
+  document.body.classList.add(background);
   return (
     <>
       <header className='primary-header d-flex'>
         <div>
           <img src={logo} alt='space tourism logo' className='logo' />
         </div>
-
+        <button
+          className='mobile-nav-toggle'
+          aria-controls='primary-navigation'
+          onClick={() => setDataisible(!dataVisible)}
+        >
+          <span className='sr-only' aria-expanded='true'>
+            Menu
+          </span>
+        </button>
         <nav>
           <ul
             id='primary-navigation'
-            className='primary-navigation d-flex ff-sans-cond'
-            data-visible='false'
+            className='primary-navigation d-flex underline-indicators'
+            data-visible={dataVisible}
           >
             <li>
-              <Link to={'/'}>
+              <Link to={'/'} className='uppercase letter-spacing-2 active'>
                 <span aria-hidden='true'>00</span>Home
               </Link>
             </li>
             <li>
-              <Link to={'/destination'}>
+              <Link to={'/destination'} className='uppercase letter-spacing-2'>
                 <span aria-hidden='true'>01</span>Destination
               </Link>
             </li>
             <li>
-              <Link to={'/crew'}>
+              <Link to={'/crew'} className='uppercase letter-spacing-2'>
                 <span aria-hidden='true'>02</span>Crew
               </Link>
             </li>
             <li>
-              <Link to={'/technology'}>
+              <Link to={'/technology'} className='uppercase letter-spacing-2'>
                 <span aria-hidden='true'>03</span>Technology
               </Link>
             </li>
